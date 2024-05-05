af46f8cc4eff302a2dd57c25adce44d51f4bbc55
flyinggorilla <flyinggorilla@outlook.com>
2024-05-05 16:12:38 +0200

    first commit

---
+++ esputil.cpp
@@ -0,0 +1,13 @@
+#include "esputil.h"
+#include "esp_system.h"
+
+const char *esp32_getresetreasontext(int reason)
+{
+  const char *reasons[11] = {"Reset reason can not be determined", "Reset due to power-on event", "Reset by external pin (not applicable for ESP32)", "Software reset via esp_restart",
+                              "Software reset due to exception/panic", "Reset (software or hardware) due to interrupt watchdog", "Reset due to task watchdog", "Reset due to other watchdogs",
+                              "Reset after exiting deep sleep mode", "Brownout reset (software or hardware)", "Reset over SDIO"};
+  if ((reason < 0) || (reason >= 11))
+    return "invalid reason";
+
+  return reasons[reason];
+}
---
+++ esputil.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_ESPUTIL_H_
+#define MAIN_ESPUTIL_H_
+
+const char* esp32_getresetreasontext(int reason);
+
+#endif 
